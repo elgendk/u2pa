648cd0d7ea77ed421c7a61d2462a0754f61fe0fc
elgen <elgen@localhost>
2012-02-19 01:13:22 +0100

    White Smoke!!! I can now read an MB8516 (2716 compatiple) and it frakking validates against MAME };-P

--- U2Pa/Properties/AssemblyInfo.cs
+++ U2Pa/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TopsyCore")]
+[assembly: AssemblyTitle("U2Pa")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TopsyCore")]
-[assembly: AssemblyCopyright("Copyright ©  2011")]
+[assembly: AssemblyProduct("U2Pa")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
