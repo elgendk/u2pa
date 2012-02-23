97bdf4920e5a066f9a72693ab671b7eff9894775
Morten Overgaard <30864637+elgendk@users.noreply.github.com>
2012-02-23 16:36:29 +0100

    Mostly moving code around to make it nicer. One new thing though, is the possibillity to visualize the defined Eproms to see if they look correct.

--- Lib/Eproms/EpromXml.cs
+++ Lib/Eproms/EpromXml.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Xml.Linq;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace U2Pa.Lib.Eproms
 {
   public class EpromXml : Eprom
   {
-    public static IDictionary<string, EpromXml> Specified { get; private set; } 
+    public static IDictionary<string, EpromXml> Specified { get; private set; }
     static EpromXml()
     {
       Specified = XDocument.Load("Eproms\\Eproms.xml").Descendants("Eprom").ToDictionary(
@@ -24,8 +23,9 @@ namespace U2Pa.Lib.Eproms
                  DataPins = x.Element("DataPins").Value.Split(',').Select(Int32.Parse).ToArray(),
                  EnablePins = x.Element("EnablePins").Value.Split(',').Select(Int32.Parse).ToArray(),
                  VccPins = x.Element("VccPins").Value.Split(',').Select(byte.Parse).ToArray(),
-                 VppPins = x.Element("VppPins").Value.Split(',').Select(byte.Parse).ToArray(),
                  GndPins = x.Element("GndPins").Value.Split(',').Select(byte.Parse).ToArray(),
+                 VppPins = x.Element("VppPins").Value.Split(',').Select(byte.Parse).ToArray(),
+                 ProgramPin = x.Element("ProgramPin") != null ? byte.Parse(x.Element("ProgramPin").Value) : (byte)0,
                  Notes = x.Element("Notes") != null ? x.Element("Notes").Value : null
                });
     }
