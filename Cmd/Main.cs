d9981ba2de814a56dc49b5a84397f599be6616fc
Morten Overgaard <30864637+elgendk@users.noreply.github.com>
2012-03-02 17:07:30 +0100

    Now with proper license info in all files.

--- Cmd/Main.cs
+++ Cmd/Main.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿//                             u2pa
+//
+//    A command line interface for Top Universal Programmers
+//
+//    Copyright (C) Elgen };-) aka Morten Overgaard 2012
+//
+//    This file is part of u2pa.
+//
+//    u2pa is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Foobar is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with u2pa. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,9 +34,16 @@ namespace U2Pa.Cmd
     {
       foreach (var fileName in Directory.GetFiles("help", "*.txt"))
       {
-        helpTexts.Add(
-          Path.GetFileNameWithoutExtension(fileName), 
-          File.ReadAllText(fileName));
+        using(var file = File.OpenText(fileName))
+        {
+          while(!file.EndOfStream)
+            if(file.ReadLine().StartsWith("~~~"))
+              break;
+         
+          helpTexts.Add(
+            Path.GetFileNameWithoutExtension(fileName),
+            file.ReadToEnd());
+        }
       }
     }
 
