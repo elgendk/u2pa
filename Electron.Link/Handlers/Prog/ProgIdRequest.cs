71e19ba2310337502284077ed887e8ee2df4184a
ElgenDK <elgemail@gmail.com>
2022-11-07 00:47:31 +0100

    Initial commit of Electron.GUI and Electron.Link

---
+++ Electron.Link/Handlers/Prog/ProgIdRequest.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using U2Pa.Lib;
+using U2Pa.Lib.IC;
+
+namespace U2pa.Electron.Link.Handlers.Prog
+{
+  public class ProgIdRequest : IRequest<string>
+  {
+    public class Handler : IRequestHandler<ProgIdRequest, string>
+    {
+      public async Task<string> Handle(ProgIdRequest request, CancellationToken cancellationToken)
+      {
+        var shouter = new Shouter(0);
+        return await Task.FromResult(TopDevice.ReadTopDeviceIdString(shouter));
+        //return EpromXml.Specified["2716"].ToString();
+      }
+    }
+  }
+}
